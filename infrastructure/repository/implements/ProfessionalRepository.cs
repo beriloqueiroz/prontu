5fc03a199874d6facc60d10da2abf8335dfdd030
berilo queiroz <luiz.berilo@buson.com.br>
2023-10-09 08:59:49 -0300

    inplementando repositoru professional

--- infrastructure/repository/implements/ProfessionalRepository.cs
+++ infrastructure/repository/implements/ProfessionalRepository.cs
@@ -1,32 +1,45 @@
 using application;
 using application.professional;
-using domain;
+using infrastructure.repository;
 
 namespace repository;
 
 public class ProfessionalRepository : IProfessionalGateway
 {
-  public void Create(Professional entity)
+  private readonly ProntuDbContext Context;
+
+  public ProfessionalRepository(ProntuDbContext context)
+  {
+    Context = context;
+  }
+  public void Create(domain.Professional entity)
   {
     throw new NotImplementedException();
   }
 
-  public Professional? Find(string id)
+  public domain.Professional? Find(string id)
   {
-    return new Professional("123654789", "Fulano de tal", "fulano.tal@gmail.com", new Cpf("74838333005"), new(), id);
+    return Context.Professionals?.Find(id)?.ToEntity();
   }
 
   public bool IsExists(string document, string email)
   {
-    throw new NotImplementedException();
+    var result = Context.Professionals?.Any(p => p.Document.Equals(document) || p.Email.Equals(email));
+    return result != null && (bool)result;
   }
 
-  public PaginatedList<Professional> List(PageAble pageAble)
+  public PaginatedList<domain.Professional> List(PageAble pageAble)
   {
-    throw new NotImplementedException();
+    if (Context.Professionals == null)
+    {
+      return PaginatedList<domain.Professional>.Empty();
+    }
+    return new(Context.Professionals
+      .Skip(pageAble.PageIndex)
+      .Take(pageAble.PageSize).ToList().Select(p => p.ToEntity()), pageAble.PageIndex, pageAble.PageSize);
   }
 
-  public void Update(Professional entity)
+  public void Update(domain.Professional entity)
   {
     throw new NotImplementedException();
   }
